995828e66f497e843734f76481dc3b19498aa4a8
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-02-27 13:51:44 +0700

    gg

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    if(argc > 1){
+        double aver;
+        cout << "---------------------------------\n";
+        for(int i=1; i < argc;i++){
+            aver += atoi(argv[i]); 
+        }
+        aver = aver/(argc-1);
+        cout << "Average of " << argc-1 << " numbers = " << aver << endl;
+        cout << "---------------------------------\n";
+    }else{
+        cout << "Please input numbers to find average.";
+    }
+}
